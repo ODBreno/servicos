9987c245b25d124154a649ce6e1707379fd56201
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2024-06-13 22:03:09 -0300

    [ADD] Vagas antigas e mais infos

--- servicos.py
+++ servicos.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify, request
-from models import db, Cliente, Vaga
-from werkzeug.security import check_password_hash
+from models import db, Cliente, Vaga, Rua, Cidade
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:brenodias@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '987654321'
 
 db.init_app(app)
 
@@ -45,7 +43,6 @@ def login():
     
     email = data['email']
     senha = data['senha']
-    placa_do_carro = data.get('placaDoCarro')  # Pegando a placa do carro do corpo da requisição
     
     print('Login Email:', email, 'Senha:', senha)
     
@@ -53,7 +50,7 @@ def login():
     if not cliente:
         return jsonify({'message': 'Email não encontrado no sistema.'}), 401
     
-    if cliente.senha!=senha:
+    if cliente.senha != senha:
         return jsonify({'message': 'Senha incorreta.'}), 401
     
     response = {'message': 'Login realizado com sucesso!', 'placaDoCarro': cliente.placadocarro}
@@ -68,22 +65,49 @@ def get_active_spot():
 
     current_time = datetime.now()
 
-    # Consulta a vaga ativa do cliente
-    active_spot = Vaga.query.filter(
+    # Consulta a vaga ativa do cliente com JOIN nas tabelas de Rua e Cidade
+    active_spot = db.session.query(Vaga, Rua, Cidade).join(Rua, Vaga.idrua == Rua.id).join(Cidade, Rua.idcidade == Cidade.id).filter(
         Vaga.placadocarro == placa_do_carro,
         Vaga.horasaida > current_time,
         Vaga.expirada == False
     ).first()
 
     if active_spot:
+        vaga, rua, cidade = active_spot
         response = {
-            'IDVaga': active_spot.idvaga,
-            'horaEntrada': active_spot.horaentrada.strftime('%Y-%m-%d %H:%M:%S'),
-            'horaSaida': active_spot.horasaida.strftime('%Y-%m-%d %H:%M:%S')
+            'horaEntrada': vaga.horaentrada.strftime('%Y-%m-%d %H:%M:%S'),
+            'horaSaida': vaga.horasaida.strftime('%Y-%m-%d %H:%M:%S'),
+            'cidade': cidade.nome,
+            'rua': rua.nome
         }
         return jsonify(response), 200
     else:
         return jsonify({'message': 'Não há vaga ativa para o cliente'}), 404
+
+@app.route('/all_spots', methods=['POST'])
+def get_all_spots():
+    data = request.get_json()
+    placa_do_carro = data.get('placaDoCarro')
+    if not placa_do_carro:
+        return jsonify({'message': 'Placa do carro não fornecida.'}), 400
+
+    # Consulta todas as vagas do cliente com JOIN nas tabelas de Rua e Cidade
+    all_spots = db.session.query(Vaga, Rua, Cidade).join(Rua, Vaga.idrua == Rua.id).join(Cidade, Rua.idcidade == Cidade.id).filter(
+        Vaga.placadocarro == placa_do_carro
+    ).all()
+
+    spots = []
+    for vaga, rua, cidade in all_spots:
+        spot_info = {
+            'horaEntrada': vaga.horaentrada.strftime('%Y-%m-%d %H:%M:%S'),
+            'horaSaida': vaga.horasaida.strftime('%Y-%m-%d %H:%M:%S'),
+            'cidade': cidade.nome,
+            'rua': rua.nome,
+            'expirada': vaga.expirada
+        }
+        spots.append(spot_info)
+
+    return jsonify(spots), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
